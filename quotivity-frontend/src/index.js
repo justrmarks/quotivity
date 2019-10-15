6231060ec8d5ced9f82d4f17c40404c7726349d4
aaron blum <blumaa@gmail.com>
2019-10-15 14:27:58 -0500

    initial front end

---
+++ quotivity-frontend/src/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('welcome to quotivity')
+  const randomButton = document.querySelector('#random-quote-button')
+  // console.log(randomButton)
+
+  document.addEventListener('click', (event) => {
+    // console.log(event.target)
+    if (event.target.id === 'random-quote-button') {
+      console.log('random quote button clicked')
+      // handleRandomQuote()
+    }
+  })
+})
